c425686095cf94c94c5f56af055e5e9e913b1dc9
INDRASISH BISWAS <indrasish.biswas2006@gmail.com>
2026-05-13 08:56:50 +0530

    Fix import case for Auth component

--- pos-frontend/src/pages/index.jsx
+++ pos-frontend/src/pages/index.jsx
@@ -1,5 +1,5 @@
 export { default as Home } from './home';
-export { default as Auth } from './Auth';
+export { default as Auth } from './auth';
 export { default as Orders } from './Orders';
 export { default as Menu } from './Menu';
 export { default as DashBoard } from './DashBoard';
